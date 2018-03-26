662e213d301e3c2c5a00afe627b362b8ff2e72fd
massimogiolitti <massimogiolitti@Massimos-iMac.local>
2018-03-26 08:49:08 -0700

    added functionality for 2 more clips. Need to merge all into one method

--- bot.rb
+++ bot.rb
@@ -2,6 +2,9 @@ require 'discordrb'
 
 bot = Discordrb::Commands::CommandBot.new token: 'NDI3NDEyODQwMTEwODgyODE2.DZqFBA.cm6skoMRN7kp20HzIunGCQ7-Of8', application_id: 427412840110882816, prefix: '!'
 
+
+#all methods will be merged into one eventually.
+
 bot.command(:gayboy_fkdem) do |event|
 	channel = event.user.voice_channel
 
@@ -14,6 +17,24 @@ bot.command(:gayboy_fkdem) do |event|
   bot.voice_connect(channel)
 	voice_bot = event.voice
 	voice_bot.play_file('/Users/massimogiolitti/desktop/pubg/data/oh_fkd_em.mp3')
+	voice_bot.destroy; nil
+end
+
+bot.command(:gayboy_lucky_boy) do |event|
+	channel = event.user.voice_channel
+  next "fuck off" unless channel
+  bot.voice_connect(channel)
+	voice_bot = event.voice
+	voice_bot.play_file('/Users/massimogiolitti/desktop/pubg/data/lucky_boy.mp3')
+	voice_bot.destroy; nil
+end
+
+bot.command(:gayboy_fkn_teammate) do |event|
+	channel = event.user.voice_channel
+  next "fuck off" unless channel
+  bot.voice_connect(channel)
+	voice_bot = event.voice
+	voice_bot.play_file('/Users/massimogiolitti/desktop/pubg/data/fkn_teammate.mp3')
 	voice_bot.destroy; nil
 end
 bot.run
