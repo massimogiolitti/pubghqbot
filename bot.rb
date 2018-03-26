95e0bc68e4c3e5d9423b60ee55cef22000d7b32c
massimogiolitti <massimogiolitti@Massimos-iMac.local>
2018-03-26 08:38:52 -0700

    first working version

--- bot.rb
+++ bot.rb
@@ -1,10 +1,19 @@
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new token: 'NDI3NDEyODQwMTEwODgyODE2.DZkf3g.GRk4nEIyykDklOxSzSnOYeuwKrQ', application_id: 427412840110882816, prefix: '!'
+bot = Discordrb::Commands::CommandBot.new token: 'NDI3NDEyODQwMTEwODgyODE2.DZqFBA.cm6skoMRN7kp20HzIunGCQ7-Of8', application_id: 427412840110882816, prefix: '!'
 
+bot.command(:gayboy_fkdem) do |event|
+	channel = event.user.voice_channel
 
-bot.command(:play_mp3) do |event|
+  # Here we return from the command unless the channel is not nil (i. e. the user is in a voice channel). The `next`
+  # construct can be used to exit a command prematurely, and even send a message while we're at it.
+  next "fuck off" unless channel
+
+  # The `voice_connect` method does everything necessary for the bot to connect to a voice channel. Afterwards the bot
+  # will be connected and ready to play stuff back.
+  bot.voice_connect(channel)
 	voice_bot = event.voice
-	voice_bot.play_file('data/music.mp3')
+	voice_bot.play_file('/Users/massimogiolitti/desktop/pubg/data/oh_fkd_em.mp3')
+	voice_bot.destroy; nil
 end
 bot.run
